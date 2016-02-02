40bc73c5cdaa4c70bad8d5183c43144018463f7b
rrauzduel <r.rauzduel@gmail.com>
2016-02-02 17:44:34 +0100

    Suppression des tests inutiles

--- src/main/java/com/company/test/BateauTest.java
+++ src/main/java/com/company/test/BateauTest.java
@@ -6,37 +6,7 @@ import org.junit.Test;
  * Created by pieagbo on 02/02/16.
  */
 public class BateauTest {
-
-    @Test
-    public void testGetTaille() throws Exception {
-
-    }
-
-    @Test
-    public void testSetTaille() throws Exception {
-
-    }
-
-    @Test
-    public void testGetNom() throws Exception {
-
-    }
-
-    @Test
-    public void testSetNom() throws Exception {
-
-    }
-
-    @Test
-    public void testGetCoordonnes() throws Exception {
-
-    }
-
-    @Test
-    public void testSetCoordonnes() throws Exception {
-
-    }
-
+    
     @Test
     public void testIsTouch() throws Exception {
 
